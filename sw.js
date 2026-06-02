ba80d15b261f20d0ea778c91667c1079d34b1023
alejodeantoni <79118071+alejodeantoni@users.noreply.github.com>
2026-06-01 21:24:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'luz-barrio-v4.2.2';
+const CACHE = 'luz-barrio-v4.2.3';
 const ASSETS = ['./', './index.html', './manifest.json', './sw.js'];
 
 self.addEventListener('install', e => {
